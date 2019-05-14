b8b5bb43d194561bd7b861659f5cd0071f697f69
Khomkrit <azimuth3d@gmail.com>
2019-05-14 20:57:06 +0700

    Add index.js file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+var deeplearn = 9
+var course = "Javascript Newbie"
+var test = 1.21232
+var JS
+var newbie = null
+var chrome = { type: "browser" }
+var isTrue = false
+
+console.log(`deeplearn is a [${typeof deeplearn}], type value is ${deeplearn}`)
+console.log(`course is a [${typeof course}], type value is "${course}"`)
+console.log(`test is a [${typeof test}] ,type value is ${test}`)
+console.log(`newbie is a [${typeof newbie}] ,type value is ${newbie}`)
+console.log(`JS is a [${typeof JS}] type, value is ${JS}`)
+console.log(
+  `chrome is a [${typeof chrome}] type , value is ${JSON.stringify(chrome)}`
+)
+console.log(`isTrue is a [${typeof isTrue}] type , value is ${isTrue}`)
