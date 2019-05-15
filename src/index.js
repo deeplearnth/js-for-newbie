188616971fecde2df884d06718e4ab7a126c8607
Khomkrit <azimuth3d@gmail.com>
2019-05-15 11:24:58 +0700

    Complete unit 4

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//JS function
+
+function getDivision(base, divide) {
+  return base / divide
+}
+
+console.log(getDivision(2, 10))
+
+const arrayOfValue = [10, 20, 30, 40]
+
+averageValue(arrayOfValue, callback)
+
+function averageValue(values, callback) {
+  const result = values.reduce((a, b) => a + b, 0) / values.length
+  callback(result)
+}
+
+function callback(result) {
+  console.log(`callback receive value ${result}`)
+}
