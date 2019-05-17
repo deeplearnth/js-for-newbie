255b396680047e17b507d24292fa33bf90244952
Khomkrit <azimuth3d@gmail.com>
2019-05-17 15:27:55 +0700

    Add more exampleon hoisting comparison

--- src/index.js
+++ src/index.js
@@ -10,11 +10,15 @@ const arrayOfValue = [10, 20, 30, 40]
 
 averageValue(arrayOfValue, getResult)
 
-function averageValue(values, callback) {
+/* function averageValue(values, callback) {
   const result = values.reduce((a, b) => a + b, 0) / values.length
   callback(result)
 }
-
+*/
+const averageValue = (values, callback) => {
+  const result = values.reduce((a, b) => a + b, 0) / values.length
+  callback(result)
+}
 function getResult(result) {
   console.log(`callback receive value ${result}`)
 }
