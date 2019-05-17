5f9d17f805de136bbafd4c9fe7f0a30dec6b7430
Khomkrit <azimuth3d@gmail.com>
2019-05-17 14:23:55 +0700

    Add loop with let example

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var result = 2 * PI * Radius
 console.log(`result is ${result}`)
 
 function powerByTwo() {
-  var result = 10
+  let result = 10
   console.log(`In function result is ${result}`)
   return result ** 2
 }
@@ -15,6 +15,14 @@ const resultPower = powerByTwo()
 console.log(`result value is ${result}`)
 console.log(`result of power function ${resultPower}`)
 
+for (var i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+// var i = 100
+console.log(i)
+/*
 console.log(hoist)
 var hoist
 let hoist
+*/
